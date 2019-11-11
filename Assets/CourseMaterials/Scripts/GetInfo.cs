920825009a42b2501d5f3feb965d0ed743f11ee3
Gustavo Bañuelos Ochoa <banuelos.201021581@gmail.com>
2019-11-11 13:42:39 -0600

    Se inicio proyecto

---
+++ Assets/CourseMaterials/Scripts/GetInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInfo : MonoBehaviour {
+
+    // Use this for initialization
+
+    public string Message;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnMouseDown()
+    {
+
+      GameObject.FindGameObjectWithTag("Finish").transform.Find("Infopanel").gameObject.SetActive(true);
+      GameObject.FindGameObjectWithTag("Finish").transform.Find("Infopanel").Find("Text").GetComponent<Text>().text = Message;
+
+    }
+}
